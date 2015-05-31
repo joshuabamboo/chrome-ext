4f81bc03fdb4c275555ef449cfa971090a8d10fb
joshuabamboo <joshuaowens011@gmail.com>
2015-05-31 17:14:53 -0400

    convert obama to a pimp named slickback

--- replace.js
+++ replace.js
@@ -1,4 +1,4 @@
 // document.body.innerHTML = document.body.innerHTML.replace(new RegExp(/Hello/ig, "g"), "yo");
 
-var replaced = $("body").html().replace(/barack obama/ig,'A Pimp Named Slickback');
+var replaced = $("body").html().replace(/barack obama|president obama|Barack Hussein Obama|obama/ig,'A Pimp Named Slickback');
 $("body").html(replaced);
